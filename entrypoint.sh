87af6431c997edd90197b1cbe7645c06c2f2b7f2
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-01-23 17:16:33 +0100

    feat: alias
    * add alias parameter
    
    * add alias to netlify command
    
    * docs: mention new arg in readme
    
    * Update README.md
    
    * Update README.md

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ BUILD_DIRECTORY=$4
 FUNCTIONS_DIRECTORY=$5
 INSTALL_COMMAND=$6
 BUILD_COMMAND=$7
+DEPLOY_ALIAS=$8
 
 # Install dependencies
 eval ${INSTALL_COMMAND:-"npm i"}
@@ -20,10 +21,17 @@ eval ${BUILD_COMMAND:-"npm run build"}
 export NETLIFY_SITE_ID=$NETLIFY_SITE_ID
 export NETLIFY_AUTH_TOKEN=$NETLIFY_AUTH_TOKEN
 
-# Deploy with netlify
+COMMAND="netlify deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message=\"$INPUT_NETLIFY_DEPLOY_MESSAGE\""
+
 if [[ $NETLIFY_DEPLOY_TO_PROD == "true" ]]
 then
-	netlify deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --prod --message="$INPUT_NETLIFY_DEPLOY_MESSAGE"
-else
-	netlify deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message="$INPUT_NETLIFY_DEPLOY_MESSAGE"
+	COMMAND+=" --prod"
 fi
+
+if [[ -n $DEPLOY_ALIAS ]]
+then
+	COMMAND+=" --alias $COMMAND"
+fi
+
+# Deploy with netlify
+eval $COMMAND
