9d9a0e534ecb257d166a9068d97c90d4921f6afd
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-11-06 12:12:54 +0100

    Install node using nvm

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Install netlify globally before NVM to prevent EACCESS issues
 npm i -g netlify-cli
+
+# Save its exec path to run later
+NETLIFY_CLI=$(which netlify)
+
+# Install node from NVM to honor .nvmrc files
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+[ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
+
+nvm install
 
 NETLIFY_AUTH_TOKEN=$1
 NETLIFY_SITE_ID=$2
@@ -29,7 +39,7 @@ eval ${BUILD_COMMAND:-"npm run build"}
 export NETLIFY_SITE_ID=$NETLIFY_SITE_ID
 export NETLIFY_AUTH_TOKEN=$NETLIFY_AUTH_TOKEN
 
-COMMAND="netlify deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message=\"$INPUT_NETLIFY_DEPLOY_MESSAGE\""
+COMMAND="$NETLIFY_CLI deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message=\"$INPUT_NETLIFY_DEPLOY_MESSAGE\""
 
 if [[ $NETLIFY_DEPLOY_TO_PROD == "true" ]]
 then
