8cf69692619c347198331f3022c8044858caafab
Cameron <561860+wallies@users.noreply.github.com>
2021-11-13 12:06:41 +1000

    Add command output

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,4 +50,14 @@ then
 fi
 
 # Deploy with netlify
-eval $COMMAND
+OUTPUT=$(sh -c "$COMMAND")
+
+NETLIFY_OUTPUT=$(echo "$OUTPUT")
+NETLIFY_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*(--)[a-zA-Z0-9./?=_-]*') #Unique key: --
+NETLIFY_LOGS_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://app.netlify.com/[a-zA-Z0-9./?=_-]*') #Unique key: app.netlify.com
+NETLIFY_LIVE_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' | grep -Eov "netlify.com") #Unique key: don't containr -- and app.netlify.com
+
+echo "::set-output name=NETLIFY_OUTPUT::$NETLIFY_OUTPUT"
+echo "::set-output name=NETLIFY_URL::$NETLIFY_URL"
+echo "::set-output name=NETLIFY_LOGS_URL::$NETLIFY_LOGS_URL"
+echo "::set-output name=NETLIFY_LIVE_URL::$NETLIFY_LIVE_URL"
