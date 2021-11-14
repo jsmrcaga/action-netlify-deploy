9cc40dcd499dd1511b3cc99912444f8970411cc6
Cameron <561860+wallies@users.noreply.github.com>
2021-11-14 21:20:52 +1000

    feat: Add Netlify output as action outputs
    * Add command output
    
    * add to contrib and readme
    
    * fix name in action

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,8 +36,8 @@ fi
 eval ${BUILD_COMMAND:-"npm run build"}
 
 # Export token to use with netlify's cli
-export NETLIFY_SITE_ID=$NETLIFY_SITE_ID
-export NETLIFY_AUTH_TOKEN=$NETLIFY_AUTH_TOKEN
+export NETLIFY_SITE_ID="$NETLIFY_SITE_ID"
+export NETLIFY_AUTH_TOKEN="$NETLIFY_AUTH_TOKEN"
 
 COMMAND="$NETLIFY_CLI deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message=\"$INPUT_NETLIFY_DEPLOY_MESSAGE\""
 
@@ -50,4 +50,14 @@ then
 fi
 
 # Deploy with netlify
-eval $COMMAND
+OUTPUT=$(sh -c "$COMMAND")
+
+NETLIFY_OUTPUT=$(echo "$OUTPUT")
+NETLIFY_PREVIEW_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*(--)[a-zA-Z0-9./?=_-]*') #Unique key: --
+NETLIFY_LOGS_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://app.netlify.com/[a-zA-Z0-9./?=_-]*') #Unique key: app.netlify.com
+NETLIFY_LIVE_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' | grep -Eov "netlify.com") #Unique key: don't containr -- and app.netlify.com
+
+echo "::set-output name=NETLIFY_OUTPUT::$NETLIFY_OUTPUT"
+echo "::set-output name=NETLIFY_PREVIEW_URL::$NETLIFY_PREVIEW_URL"
+echo "::set-output name=NETLIFY_LOGS_URL::$NETLIFY_LOGS_URL"
+echo "::set-output name=NETLIFY_LIVE_URL::$NETLIFY_LIVE_URL"
